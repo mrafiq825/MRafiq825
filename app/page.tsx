dbf48f00e0b8cd8a08a260cd4eab177904528bfa
Rafiqdevhub <rafiq85825@gmail.com>
2026-07-07 08:24:32 +0500

    Fix the structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import HomeClient from "./HomeClient";
+import HomeClient from "@/components/layout/HomeClient";
 
 export const metadata: Metadata = {
   title: "Muhammad Rafiq | Full Stack Developer & Software Engineer",
